f68781241cdcc5ab7aebfb9e24291e00f33a24bc
Rose Martus <martus.rose@gmail.com>
2021-03-27 17:34:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = new Object({JukeBox: 'Fred Astaire'})
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
 }
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName
+}
